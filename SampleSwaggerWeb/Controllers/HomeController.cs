caec3554c7d864f5ce800a72ad88d8109cf79a66
Praveena Prosper <p.prosper@metricgroup.co.uk>
2024-04-29 12:59:36 +0100

    Added new web project; Installed Nswag.APIDescriptionClient and Newtonsoft packages; Copied Swagger.json from Web api apllication to this project; Access the login API in Index()

---
+++ SampleSwaggerWeb/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleSwagger.Endpoints;
+using SampleSwaggerWeb.Models;
+using System.Diagnostics;
+
+namespace SampleSwaggerWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var client = new HttpClient();
+            var swaggerApiClient = new SwaggerAPIClient("https://localhost:7150/", client);
+
+            var loginViewModel = new LoginRequest { Username = "admin", Password = "admin" };
+            swaggerApiClient.LoginAsync(loginViewModel);
+            //var loggedInUser = swaggerApiClient.LoginAsync(loginViewModel);
+            //var token = "Bearer " + loggedInUser.Token;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SampleSwaggerWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleSwaggerWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
